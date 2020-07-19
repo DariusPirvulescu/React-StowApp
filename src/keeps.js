b9af30e3f9fb7cb5b48a6308e815bad959735587
DariusPirvulescu <organicdarius@gmail.com>
2020-07-19 22:20:29 +0100

    added keeps.js with placeholders

---
+++ src/keeps.js
@@ -0,0 +1,26 @@
+const keeps = [
+  {
+    key: 1,
+    title: 'Fill text',
+    content:
+        'Add Sagan Ipsum to all the placeholder notes',
+  },
+  {
+    key: 2,
+    title: 'Hydrogen',
+    content:
+        'Hydrogen atoms another world two ghostly white figures in coveralls and helmets are softly dancing inconspicuous motes of rock and gas Sea of Tranquility great turbulent clouds.',
+  },
+  {
+    key: 3,
+    title: 'dot',
+    content:
+        'Preserve and cherish that pale blue dot a mote of dust suspended in a sunbeam not a sunrise but a galaxyrise Cambrian explosion consciousness prime number.',
+  },
+  {
+    key: 4,
+    title: 'Tunguska',
+    content:
+        'Tunguska event citizens of distant epochs billions upon billions hydrogen atoms finite but unbounded brain is the seed of intelligence.',
+  },
+];
