201edb827f2aa0b37c1696c4bc00f7a2de36640b
DariusPirvulescu <organicdarius@gmail.com>
2020-07-31 23:53:17 +0300

    wrote the add new note logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import keeps from '../keeps';
 import NoteForm from './NoteForm';
 
-const createNotes = (keep) => (
-  <Note
-    key={keep.key}
-    title={keep.title}
-    content={keep.content}
-  />
-);
+const App = () => {
+  const [notesArr, setNotesArr] = useState([]);
 
-const App = () => (
-  <div>
-    <Header />
-    <NoteForm />
-    <div className="notes-area">
-      { keeps.map(createNotes) }
+  const createNotes = (keep) => (
+    <Note
+      key={keep.key}
+      title={keep.title}
+      content={keep.content}
+    />
+  );
+  
+  const handleSubmit = (title, text) => {
+    const newNote = (
+      <Note
+        key={Math.floor(Math.random() * 70)}
+        title={title}
+        content={text}
+      />
+    );
+    setNotesArr((prevValue) => {
+      return [...prevValue, newNote]
+    });
+    console.log(notesArr);
+  };
+
+  return (
+    <div>
+      <Header />
+      <NoteForm onSubmit={handleSubmit} />
+      <div className="notes-area">
+        {/* { keeps.map(createNotes) } */}
+        {/* {notesArr.forEach()} */}
+        {notesArr}
+        <Note />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+};
 
 export default App;
--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NoteFrom = () => (
-  <div>
-    <form className="note-form">
-      <input name="title" placeholder="Title" />
-      <textarea name="content" placeholder="Take a note..." rows="3" />
-      <button>Add</button>
-    </form>
-  </div>
-);
+const NoteFrom = (props) => {
+  const [noteTitle, setNoteTitle] = useState('');
+  const [noteText, setNoteText] = useState('');
+
+  const handleTitle = (e) => {
+    const title = e.target.value;
+    setNoteTitle(title);
+  };
+
+  const handleText = (e) => {
+    const text = e.target.value;
+    setNoteText(text);
+  };
+
+  return( 
+    <div>
+      <form 
+      onSubmit={(e) => {
+        props.onSubmit(noteTitle, noteText)
+        e.preventDefault();
+        setNoteTitle('')
+        setNoteText('')
+      }} 
+      className="note-form">
+        <input onChange={handleTitle} value={noteTitle} name="title" placeholder="Title" />
+        <textarea onChange={handleText} value={noteText} name="content" placeholder="Take a note..." rows="3" />
+        <button>Add</button>
+      </form>
+    </div>
+  )
+};
 
 export default NoteFrom;
