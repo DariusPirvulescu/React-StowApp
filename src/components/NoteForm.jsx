7ea885d7523f20a274fba2511a03d048ce3f8141
DariusPirvulescu <organicdarius@gmail.com>
2020-08-01 00:22:54 +0300

    combined the note's title and text into one object state

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,35 +1,38 @@
+/* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
 
 const NoteFrom = (props) => {
-  const [noteTitle, setNoteTitle] = useState('');
-  const [noteText, setNoteText] = useState('');
+  const [note, setNote] = useState({
+    title: '',
+    text: ''
+  });
 
-  const handleTitle = (e) => {
-    const title = e.target.value;
-    setNoteTitle(title);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
   };
 
-  const handleText = (e) => {
-    const text = e.target.value;
-    setNoteText(text);
-  };
-
-  return( 
+  return (
     <div>
-      <form 
-      onSubmit={(e) => {
-        props.onSubmit(noteTitle, noteText)
-        e.preventDefault();
-        setNoteTitle('')
-        setNoteText('')
-      }} 
-      className="note-form">
-        <input onChange={handleTitle} value={noteTitle} name="title" placeholder="Title" />
-        <textarea onChange={handleText} value={noteText} name="content" placeholder="Take a note..." rows="3" />
+      <form
+        onSubmit={(e) => {
+          props.onSubmit(note.title, note.text);
+          e.preventDefault();
+          setNote({ title: '', text: '' });
+        }}
+        className="note-form"
+      >
+        <input onChange={handleChange} value={note.title} name="title" placeholder="Title" />
+        <textarea onChange={handleChange} value={note.text} name="text" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
-  )
+  );
 };
 
 export default NoteFrom;
