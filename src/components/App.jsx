ef3a0c08e4fb719ed1cddc07b96d690c367ee68e
DariusPirvulescu <organicdarius@gmail.com>
2020-07-31 22:30:47 +0300

    changed the function syntax

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,23 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import keeps from '../keeps';
-import NoteForm from "./NoteForm";
+import NoteForm from './NoteForm';
 
-function createNotes(keep) {
-  return (
-    <Note
-      key={keep.key}
-      title={keep.title}
-      content={keep.content}
-    />
-  );
-};
+const createNotes = (keep) => (
+  <Note
+    key={keep.key}
+    title={keep.title}
+    content={keep.content}
+  />
+);
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <NoteForm />
-      { keeps.map(createNotes) }
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <Header />
+    <NoteForm />
+    { keeps.map(createNotes) }
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-function Note(props) {
-  return (
-    <div className="note">
-      {/* <h1>Note title</h1>
+const Note = (props) => (
+  <div className="note">
+    {/* <h1>Note title</h1>
       <p>Note description</p> */}
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-    </div>
-  );
-}
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
+  </div>
+);
 
 export default Note;
