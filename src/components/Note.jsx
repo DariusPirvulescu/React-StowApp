ea87543e129dd4376fcec38ced6031c417f2162f
DariusPirvulescu <organicdarius@gmail.com>
2020-08-01 20:51:41 +0300

    added remove icon to notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const Note = (props) => (
   <div className="note">
-    {/* <h1>Note title</h1>
-      <p>Note description</p> */}
-
     <h1>{props.title}</h1>
     <p>{props.content}</p>
     <button
+      // type="button"
       onClick={() => {
         props.onComplete(props.id);
       }}
       className="delete-btn"
     >
-      Delete
+      <HighlightOffIcon />
     </button>
   </div>
 );
