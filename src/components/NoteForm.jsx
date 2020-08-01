7044472c590ea4823f2a3a72173b040a1a48c127
DariusPirvulescu <organicdarius@gmail.com>
2020-08-01 20:52:19 +0300

    added conditional rendering for NoteForm only when you click the input

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
+import Zoom from '@material-ui/core/Zoom';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 const NoteFrom = (props) => {
   const [note, setNote] = useState({
     title: '',
     text: ''
   });
+  const [expanded, setExpanded] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,22 +21,30 @@ const NoteFrom = (props) => {
     });
   };
 
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          props.onSubmit(note);
-          e.preventDefault();
-          setNote({ title: '', text: '' });
-        }}
-        className="note-form"
-      >
-        <input onChange={handleChange} value={note.title} name="title" placeholder="Title" />
-        <textarea onChange={handleChange} value={note.text} name="text" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+  const handleFocus = () => {
+    setExpanded(true);
+  };
+  
+    return (
+      <div>
+        <form
+          onSubmit={(e) => {
+            props.onSubmit(note);
+            e.preventDefault();
+            setNote({ title: '', text: '' });
+          }}
+          className="note-form"
+        >
+          {expanded ? <input onChange={handleChange} value={note.title} name="title" placeholder="Title" /> : null }
+          <textarea onFocus={handleFocus} onChange={handleChange} value={note.text} name="text" placeholder="Take a note..." rows={expanded ? 3 : 1} />
+          <Zoom in={expanded}>
+            <Fab>
+              <AddIcon />
+            </Fab>
+          </Zoom>
+        </form>
+      </div>
+    );
 };
 
 export default NoteFrom;
