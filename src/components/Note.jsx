455335665e7035d1222e23eea668bb19b87d37a1
DariusPirvulescu <organicdarius@gmail.com>
2020-08-01 18:52:48 +0300

    added delete btn for each note + basic style

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,8 +4,10 @@ const Note = (props) => (
   <div className="note">
     {/* <h1>Note title</h1>
       <p>Note description</p> */}
+    
     <h1>{props.title}</h1>
     <p>{props.content}</p>
+    <button className="delete-btn">Delete</button>
   </div>
 );
 
