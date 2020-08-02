2429625a20fe6d4bfe942d74b85d5e93e07630e1
DariusPirvulescu <organicdarius@gmail.com>
2020-08-02 21:43:31 +0300

    added type attribute to button

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
-const Note = (props) => (
-  <div className="note">
-    <h1>{props.title}</h1>
-    <p>{props.content}</p>
-    <button
-      // type="button"
-      onClick={() => {
-        props.onComplete(props.id);
-      }}
-      className="delete-btn"
-    >
-      <HighlightOffIcon />
-    </button>
-  </div>
-);
+const Note = (props) => {
+  const handleDelete = () => {
+    props.onComplete(props.id);
+  };
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button
+        type="button"
+        onClick={handleDelete}
+        className="delete-btn"
+      >
+        <HighlightOffIcon />
+      </button>
+    </div>
+  );
+};
 
 export default Note;
